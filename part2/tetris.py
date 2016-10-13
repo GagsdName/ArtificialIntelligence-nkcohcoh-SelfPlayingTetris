e0d966c467bfa98efe745198c955fbd22c8e0c73
singhgag <singhgag@indiana.edu>
2016-10-13 03:34:44 -0400

    expectiminimax start

--- part2/tetris.py
+++ part2/tetris.py
@@ -23,47 +23,88 @@ class HumanPlayer:
 # Replace our super simple algorithm with something better
 #
 class ComputerPlayer:
-    # Given a new piece (encoded as a list of strings) and a board (also list of strings), 
-    # this function should generate a series of commands to move the piece into the "optimal"
-    # position. The commands are a string of letters, where b and m represent left and right, respectively,
-    # and n rotates. 
-    #
-    def get_moves(self, piece, board):
-        # super simple current algorithm: just randomly move left, right, and rotate a few times
-        return random.choice("mnb") * random.randint(1, 10)
+   
+	#depth = 0
+	#desired_depth = 6
+	commands = [ "b", "n", "m"]
+	#chance node
+        def chance_layer(self,piece,actions, board):
+		new_position = self.result(piece,actions,board)
+		return True
+	#max node value
+        def max_layer(self,piece,board):
+		max_val = 0
+		for action1 in self.commands:
+			for action2 in self.commands:
+				for action3 in self.commands:
+					print (action1,action2,action3)
+					chance_eval = self.chance_layer(piece,(action1,action2,action3), board)
+					if chance_eval > max_val:
+						max_val = chance_eval
+                	
+		print max_val
+		return True
+	#ExpectiMiniMax
+	def get_choice(self,piece,board):
+		desired_action = self.max_layer(piece,board)
+		return desired_action 
+
+	#evaluation function for a given state
+	def evaluation(self,position):
+        	print tetris.get_board()	
+	
+	#transition model - result of an action on the current state
+	def result(self,piece,actions,board):
+		print "\nactions:",actions
+		print "\nBefore Actions:", tetris.get_piece()
+		commands_map = { "b": tetris.left, "n": tetris.rotate, "m": tetris.right, " ": tetris.down }
+		for action in actions:
+			commands_map[action]()
+			self.evaluation(tetris.get_piece())			
+		return True
+
+	# Given a new piece (encoded as a list of strings) and a board (also list of strings), 
+    	# this function should generate a series of commands to move the piece into the "optimal"
+    	# position. The commands are a string of letters, where b and m represent left and right, respectively,
+    	# and n rotates. 
+    	#
+    	def get_moves(self, piece, board):
+        	move_string = self.get_choice(piece, board)
+		#print self
+		# super simple current algorithm: just randomly move left, right, and rotate a few times
+        	return random.choice("mnb") * random.randint(1, 10)
        
-    # This is the version that's used by the animted version. This is really similar to get_moves,
-    # except that it runs as a separate thread and you should access various methods and data in
-    # the "tetris" object to control the movement. In particular:
-    #   - tetris.col, tetris.row have the current column and row of the upper-left corner of the 
-    #     falling piece
-    #   - tetris.get_piece() is the current piece, tetris.get_next_piece() is the next piece after that
-    #   - tetris.left(), tetris.right(), tetris.down(), and tetris.rotate() can be called to actually
-    #     issue game commands
-    #   - tetris.get_board() returns the current state of the board, as a list of strings.
-    #
-    def control_game(self, tetris):
-        # another super simple algorithm: just move piece to the least-full column
-        while 1:
-            time.sleep(0.1)
+    	# This is the version that's used by the animted version. This is really similar to get_moves,
+    	# except that it runs as a separate thread and you should access various methods and data in
+    	# the "tetris" object to control the movement. In particular:
+    	#   - tetris.col, tetris.row have the current column and row of the upper-left corner of the 
+    	#     falling piece
+    	#   - tetris.get_piece() is the current piece, tetris.get_next_piece() is the next piece after that
+    	#   - tetris.left(), tetris.right(), tetris.down(), and tetris.rotate() can be called to actually
+    	#     issue game commands
+    	#   - tetris.get_board() returns the current state of the board, as a list of strings.
+    	#
+    	def control_game(self, tetris):
+        	# another super simple algorithm: just move piece to the least-full column
+        	while 1:
+            		time.sleep(0.1)
 
-            board = tetris.get_board()
-            column_heights = [ min([ r for r in range(len(board)-1, 0, -1) if board[r][c] == "x"  ] + [100,] ) for c in range(0, len(board[0]) ) ]
-            index = column_heights.index(max(column_heights))
+            		board = tetris.get_board()
+            		column_heights = [ min([ r for r in range(len(board)-1, 0, -1) if board[r][c] == "x"  ] + [100,] ) for c in range(0, len(board[0]) ) ]
+            		index = column_heights.index(max(column_heights))
 
-            if(index < tetris.col):
-                tetris.left()
-            elif(index > tetris.col):
-                tetris.right()
-            else:
-                tetris.down()
-
+            		if(index < tetris.col):
+                		tetris.left()
+            		elif(index > tetris.col):
+                		tetris.right()
+            		else:
+                		tetris.down()
+    
 
 ###################
 #### main program
 
 (player_opt, interface_opt) = sys.argv[1:3]
-
 try:
     if player_opt == "human":
         player = HumanPlayer()
@@ -78,7 +119,6 @@ try:
         tetris = AnimatedTetris()
     else:
         print "unknown interface!"
-
     tetris.start_game(player)
 
 except EndOfGame as s:
